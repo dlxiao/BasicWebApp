8cd7bf6ea0efe58f8bcb83315413c1a58519c9ac
Dora Xiao <dlxiao@LAPTOP-DUF7JNPL.localdomain>
2022-09-22 11:22:19 -0400

    Fixed largest query, the  errors thrown

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -20,11 +20,12 @@ public class QueryProcessor {
 
         if(query.toLowerCase().contains("largest")) {
             String[] parts = query.split(",");
-            String first_num = parts[0].substring((query.indexOf(':') + 2));
+            String first_num = parts[0].substring((query.indexOf(":") + 2));
 
-            int max = Integer.parseInt(first_num);
-            for(String num_str : parts) {
-                int num_int = Integer.parseInt(num_str.substring(1));
+            int max = Integer.valueOf(first_num);
+            for(int i = 1; i < parts.length; i++) {
+                int num_int = Integer.valueOf(parts[i].substring(1));
+                System.out.println(num_int);
                 if(num_int > max) {
                     max = num_int;
                 } 
@@ -33,11 +34,6 @@ public class QueryProcessor {
         }
 
         if(query.toLowerCase().contains("plus")) {
-
-            // String input = "Today is 22 September";
-            // Pattern regex = Pattern.compile("Today is (\\d+) \\w+");
-            // Matcher matcher = regex.matcher(input);
-            // System.out.println(matcher.group(1));
             String first_num = query.substring(query.indexOf("is ") + 3, query.indexOf("plus") - 1);
             String second_num = query.substring(query.indexOf("plus ") + 5);
             int sum = Integer.valueOf(first_num) + Integer.valueOf(second_num);
--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -35,10 +35,10 @@ public class QueryProcessorTest {
         assertThat(queryProcessor.process("newquery"), containsString("New query!"));
     }
 
-    // @Test
-    // public void knowsAboutLargest() throws Exception {
-    //     assertThat(queryProcessor.process("which of the following numbers is the largest: 83, 91, 155, 918"), containsString("918"));
-    // }
+    @Test
+    public void knowsAboutLargest() throws Exception {
+        assertThat(queryProcessor.process("Which of the following numbers is the largest: 83, 91, 155, 918"), containsString("918"));
+    }
 
     @Test
     public void knowsAboutPlus() throws Exception {
